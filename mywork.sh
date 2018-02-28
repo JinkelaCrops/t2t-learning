b292ac8182c2744aef8a450469e55086647e8d9d
panyan <panyan@tmxmall.com>
2018-02-28 16:18:12 +0800

    add bash

--- mywork.sh
+++ mywork.sh
@@ -1,48 +1,22 @@
 #!/usr/bin/env bash
 
-python tensor2tensor/bin/t2t_trainer.py --registry_help
-
 HOMEPATH=../t2t_med
-PROBLEM=translate_enzh_med
-MODEL=transformer
-HPARAMS=transformer_base_single_gpu
+TMP_DIR=$HOMEPATH/t2t_datagen/medicine
 
-DATA_DIR=$HOMEPATH/t2t_data
-TMP_DIR=$HOMEPATH/t2t_datagen
-TRAIN_DIR=$HOMEPATH/t2t_train/$PROBLEM/$MODEL-$HPARAMS
+# filter
+python filter.py -f $TMP_DIR/medicine.sample.txt -s " ||| "
 
-mkdir -p $DATA_DIR $TMP_DIR $TRAIN_DIR
+# unpack
+python unpack.py -f $TMP_DIR/filter.medicine.sample.txt -s " ||| "
 
-echo "Generate data"
-t2t-datagen --data_dir=$DATA_DIR --tmp_dir=$TMP_DIR --problem=$PROBLEM
+# segment
+python segment.py -f $TMP_DIR/filter.medicine.sample.txt.zh -l zh
+python segment.py -f $TMP_DIR/filter.medicine.sample.txt.en -l en
 
-echo "Train,* If you run out of memory, add --hparams='batch_size=1024'."
-t2t-trainer --data_dir=$DATA_DIR --problems=$PROBLEM --model=$MODEL --hparams_set=$HPARAMS --output_dir=$TRAIN_DIR \
-            --
+# trainvalidsplit
+python trainvalidsplit.py -f $TMP_DIR/seg.filter.medicine.sample.txt -z 1000
 
-# Decode
+tar -cvPf $TMP_DIR/medicine.tar.gz $TMP_DIR/train.en $TMP_DIR/train.zh $TMP_DIR/valid.en $TMP_DIR/valid.zh
 
-DECODE_FILE=$DATA_DIR/decode_this.txt
-echo "Hello world" >> $DECODE_FILE
-echo "Goodbye world" >> $DECODE_FILE
-echo -e 'Hallo Welt\nAuf Wiedersehen Welt' > ref-translation.de
 
-BEAM_SIZE=4
-ALPHA=0.6
 
-t2t-decoder \
-  --data_dir=$DATA_DIR \
-  --problems=$PROBLEM \
-  --model=$MODEL \
-  --hparams_set=$HPARAMS \
-  --output_dir=$TRAIN_DIR \
-  --decode_hparams="beam_size=$BEAM_SIZE,alpha=$ALPHA" \
-  --decode_from_file=$DECODE_FILE \
-  --decode_to_file=translation.en
-
-# See the translations
-cat translation.en
-
-# Evaluate the BLEU score
-# Note: Report this BLEU score in papers, not the internal approx_bleu metric.
-t2t-bleu --translation=translation.en --reference=ref-translation.de
