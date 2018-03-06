229179787c6d25efd0c02657efe61fd0f92824d7
yanpan-ubuntu <1499059470@qq.com>
2018-03-06 22:21:44 +0800

    homepath change

--- mydecoder.sh
+++ mydecoder.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-HOMEPATH=/media/tmxmall/a36811aa-0e87-4ba1-b14f-370134452449/t2t_med
-
 PROBLEM=translate_zhen_med_small_vocab
 MODEL=transformer
 HPARAMS=transformer_base_single_gpu_batch_size_4096
