8bd1bf0220062408744964f1eb4c7485243b06b0
panyan <panyan@tmxmall.com>
2018-03-07 20:23:03 +0800

    add more greedy

--- analyzerafter.sh
+++ analyzerafter.sh
@@ -9,13 +9,13 @@ DATA_DIR=$HOMEPATH/t2t_data/new_medicine
 TRAIN_DIR=$HOMEPATH/t2t_train/new_medicine/$PROBLEM/$MODEL-$HPARAMS
 
 # Decode
-DECODE_FILE=$TMP_DIR/med_zhen_30000k_tok_dev.lang1
-#DECODE_FILE=$HOMEPATH/t2t_datagen/med_test/seg.src.test.txt.zh
-OUTPUT_FILE=$TRAIN_DIR/translation.en.test
+#DECODE_FILE=$TMP_DIR/med_zhen_30000k_tok_dev.lang1
+DECODE_FILE=$HOMEPATH/t2t_datagen/med_test/seg.src.test.txt.decode.zh
+OUTPUT_FILE=$TRAIN_DIR/translation.en.test.srctest
 
-REF_FILE=$TMP_DIR/med_zhen_30000k_tok_dev.lang2
-#REF_FILE=$HOMEPATH/t2t_datagen/med_test/seg.src.test.txt.en
+#REF_FILE=$TMP_DIR/med_zhen_30000k_tok_dev.lang2
+REF_FILE=$HOMEPATH/t2t_datagen/med_test/seg.src.test.txt.decode.en
 TGT_FILE=$OUTPUT_FILE
 
-python analyzerdecode.py -f $OUTPUT_FILE -d $TMP_DIR/valid.dict -t $TRAIN_DIR/translation.en
-python mybleu.py -rf $TMP_DIR/src.valid.en -tf $TRAIN_DIR/translation.en -l en
+python analyzerdecode.py -f $OUTPUT_FILE -d $HOMEPATH/t2t_datagen/med_test/src.test.txt.decode.dict -t $TRAIN_DIR/translation.en.srctest
+python mybleu.py -rf $HOMEPATH/t2t_datagen/med_test/seg.encode.src.test.txt.decode.en -tf $TRAIN_DIR/translation.en.srctest -l en --lower True
--- analyzerbefore.sh
+++ analyzerbefore.sh
@@ -2,18 +2,20 @@
 
 source activate py36
 
-TASK=new_medicine
+TASK=med_test
 TMP_DIR=$HOMEPATH/t2t_datagen/$TASK
-FILE_NAME=src.test
+FILE_NAME=src.test.txt
 
-# term pool
+## term pool
 python analyzerencode.py -f $TMP_DIR/$FILE_NAME --report 100000
 
 # segment
 python segment.py -f $TMP_DIR/$FILE_NAME.decode.en -l en
 python segment.py -f $TMP_DIR/$FILE_NAME.decode.zh -l zh
 
-
+# src gen
+python analyzerdecode.py -f $TMP_DIR/$FILE_NAME.decode.en -d $TMP_DIR/$FILE_NAME.decode.en.dict -t $TMP_DIR/encode.$FILE_NAME.decode.en
+python segment.py -f $TMP_DIR/encode.$FILE_NAME.decode.en -l en
 
 
 
