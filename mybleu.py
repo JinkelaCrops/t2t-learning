ab67c3798ddaee1fcef9b45a74a877193f497fd1
panyan <panyan@tmxmall.com>
2018-03-07 14:21:07 +0800

    del TranslateZhenNewMedSmallVocabCopy

--- mybleu.py
+++ mybleu.py
@@ -7,13 +7,15 @@ parser.add_argument('-rf', "--ref_file_path")
 parser.add_argument('-tf', "--tgt_file_path")
 parser.add_argument('-l', "--language")
 parser.add_argument("--lower", default=False, type=bool)
+parser.add_argument("--truncate", default=1E10, type=int)
 
 args = parser.parse_args()
 
 
 class Decode(object):
-    def __init__(self, lower=False):
+    def __init__(self, lower=False, truncate=1E10):
         self.lower = lower
+        self.truncate = truncate
         pass
 
     @staticmethod
@@ -27,22 +29,26 @@ class Decode(object):
     def zh_decode(self, path):
         if self.lower:
             with open(path, "r", encoding="utf8") as f:
-                for line in f:
-                    yield self.zh_separator(line[:-1].lower())
+                for k, line in enumerate(f):
+                    if k < self.truncate:
+                        yield self.zh_separator(line[:-1].lower())
         else:
             with open(path, "r", encoding="utf8") as f:
-                for line in f:
-                    yield self.zh_separator(line[:-1])
+                for k, line in enumerate(f):
+                    if k < self.truncate:
+                        yield self.zh_separator(line[:-1])
 
     def en_decode(self, path):
         if self.lower:
             with open(path, "r", encoding="utf8") as f:
-                for line in f:
-                    yield self.en_separator(line[:-1].lower())
+                for k, line in enumerate(f):
+                    if k < self.truncate:
+                        yield self.en_separator(line[:-1].lower())
         else:
             with open(path, "r", encoding="utf8") as f:
-                for line in f:
-                    yield self.en_separator(line[:-1])
+                for k, line in enumerate(f):
+                    if k < self.truncate:
+                        yield self.en_separator(line[:-1])
 
 
 if __name__ == '__main__':
@@ -50,7 +56,7 @@ if __name__ == '__main__':
     tgt_path = args.tgt_file_path
     language = args.language
 
-    decode = Decode(args.lower)
+    decode = Decode(args.lower, args.truncate)
     if language == "zh":
         ref = [line.split() for line in decode.zh_decode(ref_path)]
         tgt = [line.split() for line in decode.zh_decode(tgt_path)]
@@ -59,7 +65,6 @@ if __name__ == '__main__':
         tgt = [line.split() for line in decode.en_decode(tgt_path)]
 
     print("my bleu: bleu4 is %s" % corpus_bleu([[r] for r in ref], tgt))
-
 
 # 0.17842, translate_enzh_wmt8k, transformer, transformer_base_single_gpu
 # 0.09039, translate_enzh_wmt8k, lstm_seq2seq_attention, lstm_luong_attention_multi
--- transapi.py
+++ transapi.py
@@ -1,11 +1,18 @@
 import requests
 import json
 import urllib
+import argparse
+
+parser = argparse.ArgumentParser(description="transapi.py")
+parser.add_argument('-f', "--file_path")
+parser.add_argument("--truncate", default=1E10, type=int)
+
+args = parser.parse_args()
 
 
 class TranslateAPI(object):
     url_text = lambda \
-        text: f"http://qa.tmxmall.com/v1/http/mttranslate?text={text}&user_name=1499059470@qq.com&client_id=e0c2ccfde21c5de10af7a72e53bcc273&from=zh-CN&to=en-US&de=trados"
+            text: f"http://qa.tmxmall.com/v1/http/mttranslate?text={text}&user_name=1499059470@qq.com&client_id=e0c2ccfde21c5de10af7a72e53bcc273&from=zh-CN&to=en-US&de=trados"
 
 
 def trans(src):
@@ -14,10 +21,21 @@ def trans(src):
     try:
         outcome = json.loads(r.text)
         tgt = outcome["mt_set"][0]["tgt"]
-        print(outcome["mt_set"][0]["provider"])
+        print("from:", outcome["mt_set"][0]["provider"])
         return tgt
     except Exception as e:
         print(r.text)
+        return "ERROR"
+
 
 if __name__ == '__main__':
-    trans("动物在注入LPS后 (2 0 5± 1 0 5 )h ,氧合指数 (PaO2 /FIO2 )≤ 30 0。 此时为ALI,肺组织学可见肺泡水肿 ,肺泡壁淤血 ,肺泡内可见中性粒细胞浸润。")
+
+    trans_result = []
+    with open(args.file_path, "r", encoding="utf8") as f:
+        for k, line in enumerate(f):
+            if k < args.truncate:
+                tgt = trans(line[:-1])
+                trans_result.append(tgt)
+
+    with open(args.file_path + ".fromapi", "w", encoding="utf8") as f:
+        f.writelines([x + "\n" for x in trans_result])
