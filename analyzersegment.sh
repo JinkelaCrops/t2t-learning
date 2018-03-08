e38780a10d70a0e000611a056e6947054b23373d
panyan <panyan@tmxmall.com>
2018-03-08 22:24:25 +0800

    follow the problem

--- analyzersegment.sh
+++ analyzersegment.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-TASK=med_test
-TMP_DIR=$HOMEPATH/t2t_datagen/$TASK
-FILE_NAME=google
+TMP_DIR=$HOMEPATH/t2t_datagen/med_test/valid1
+FILE_NAME=valid1.t2t_lstm_attention_bpe
 
 # term process
 python analyzerencode.py -f $TMP_DIR/$FILE_NAME --report 100000
@@ -11,5 +10,11 @@ python analyzerencode.py -f $TMP_DIR/$FILE_NAME --report 100000
 python segment.py -f $TMP_DIR/$FILE_NAME.decode -l en
 
 # src gen
-python analyzerdecode.py -f $TMP_DIR/$FILE_NAME.decode -d $TMP_DIR/$FILE_NAME.dict -t $TMP_DIR/encode.$FILE_NAME.decode
+python analyzerdecode.py -f $TMP_DIR/seg.$FILE_NAME.decode -d $TMP_DIR/$FILE_NAME.decode.dict -t $TMP_DIR/encode.seg.$FILE_NAME.decode
 
+cp $TMP_DIR/encode.seg.$FILE_NAME.decode $TMP_DIR/encode.$FILE_NAME
+
+rm $TMP_DIR/*.decode
+rm $TMP_DIR/*.dict
+
+# python mybleu.py -rf $HOMEPATH/t2t_datagen/med_test/valid1/encode.valid1 -tf $HOMEPATH/t2t_datagen/med_test/valid1/encode.valid1.google --truncate 1000 --lower True
