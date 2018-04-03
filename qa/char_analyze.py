e0a9593e7b74fc08e1d4d36538665c40c84015a8
panyan <panyan@tmxmall.com>
2018-04-03 17:34:51 +0800

    add char analyze

--- qa/char_analyze.py
+++ qa/char_analyze.py
@@ -9,390 +9,216 @@ with open(resource_path, "r", encoding="utf8") as f:
     char_stream = f.read()
 
 char_dictionary = Counter(list(char_stream))
-med_interval_exprs = list(char_dictionary.keys()) + ["\\u4e00-\\u9fff"]
-med_unicodes = interval_expr_to_unicode(med_interval_exprs)
+med_unicodes = expr_converter("[[%s]]" % "".join(char_dictionary.keys()).replace("\n", "") + "#[[\\u4e00-\\u9fff]]")
 format_med_unicodes = re.sub("(?<!-)(?=\\\\u)", "\n", med_unicodes)
 print(format_med_unicodes)
+
+lines = char_stream.split("\n")
+
+unknown_char = "[^\\u0020-\\u007e\\u4e00-\\u9fff]"
+
+
+def regex_filter_line(regex, lines):
+    filter_sentence = list(filter(lambda x: re.search(regex, x) is not None, lines))
+    print(regex, len(filter_sentence))
+    return len(filter_sentence)
+
+
+regex_filter_line("[\\u00a0-\\u00ff]", lines)
+
 """
-\\u0020-\\u002c 
-\\u002e-\\u005d
-\\u005f-\\u007e
-\\u00b0-\\u00b1
+\\u0020-\\u007e Latin
+\\u00a0-\\u00ff Latin ++
+
+\\u0100-\\u01ff Latin ++
+
+\\u0251 ɑ
+\\u025b ɛ
+\\u0261 ɡ
+\\u028a ʊ
+\\u02c6-\\u02cb ˆˇˈˉˊˋ
+\\u02d0 ː
+\\u02d8-\\u02da ˘˙˚
+\\u02dc ˜
+
+\\u037a ͺ
+\\u037e ;
+\\u038a Ί
+\\u038c Ό
+\\u03cb ϋ
+\\u03d6 ϖ
+\\u0384-\\u0385 ΄΅
+\\u0387-\\u0388 ·Έ
+\\u038e-\\u038f ΎΏ
 
 \\u0391-\\u03c9 希腊
 
-\\u2018-\\u2019 ‘’
-\\u201c-\\u201d “”
-\\u2030 ‰
-\\u2032 ′
+\\u0400-\\u04ff 西里尔
+
+\\u0590-\\u05ff 希伯来
+
+\\u0652 阿拉伯
+
+\\u11bc 朝鲜
+
+\\u1868 ᡨ 蒙古
+
+\\u1d31 ᴱ
+\\u1d52 ᵒ
+\\u1d5b ᵛ
+
+\\u1ef7 ỷ Latin ++
+
+\\u2016-\\u206a punc++
+\\u2070 ⁰
+\\u2074-\\u2075 ⁴⁵
+\\u2077-\\u2078 ⁷⁸
+\\u2082-\\u2084 ₂₃₄
+\\u20ac €
+
 \\u2103 ℃
-
+\\u2105 ℅
+\\u2109 ℉
+\\u2116 №
+\\u2122 ™
+\\u212b Å
 \\u2160-\\u216b ⅠⅡⅢⅣⅤⅥⅦⅧⅨⅩⅪⅫ
-\\u2460-\\u2461 ①②
-\\u3001-\\u3002 、。
-\\u3009-\\u300b 〈〉《》
+\\u2170-\\u2179 ⅰⅱⅲⅳⅴⅵⅶⅷⅸ
+\\u21d2 ⇒
+\\u2190-\\u2193 ←↑→↓
+
+\\u2206 ∆
+\\u2208 ∈
+\\u2211-\\u2212 ∑−
+\\u2217-\\u221a ∗∘∙√
+\\u221d-\\u2220 ∝∞∟∠
+\\u2223 ∣
+\\u2225 ∥
+\\u2227-\\u222b ∧∨∩∪∫
+\\u222e ∮
+\\u2234 ∴
+\\u2237 ∷
+\\u223c-\\u223d ∼∽
+\\u2245 ≅
+\\u224c ≌
+\\u2252 ≒
+\\u2260-\\u2261 ≠≡
+\\u2264-\\u2267 ≤≥≦≧
+\\u226f ≯
+\\u2295 ⊕
+\\u2299 ⊙
+\\u22a5 ⊥
+\\u22bf ⊿
+
+\\u2312 ⌒
+\\u2395 ⎕
+
+\\u2460-\\u2473 ①②③④⑤⑥⑦⑧⑨⑩ ⑳
+\\u2474-\\u2487 ⑴⑵⑶⑷⑸⑹⑺⑻⑼⑽ ⒇
+\\u2488-\\u249b ⒈⒉⒊⒋⒌⒍⒎⒏⒐⒑ ⒛
+
+\\u2500-\\u257f ─━│┃┄┅┆┇┈┉┊
+\\u25a0-\\u25a1 ■□
+\\u25b2-\\u25b4 ▲△▴
+\\u25c6-\\u25c7 ◆◇
+\\u25ca-\\u25cb ◊○
+\\u25ce-\\u25cf ◎●
+
+\\u2605-\\u2606 ★☆
+\\u2609 ☉
+\\u2610 ☐
+\\u2640 ♀
+\\u2642 ♂
+\\u2666 ♦
+\\u266a-\\u266b ♪♫
+
+\\u2714 ✔
+\\u2717 ✗
+\\u274f ❏
+\\u2751 ❑
+\\u279f ➟
+\\u27a2 ➢
+\\u27a5 ➥
+
+\\u2a7d ⩽
+
+\\u2fd4 ⿔ CJK++
+
+\\u3001-\\u301e CJK punc
+
+\\u3022-\\u3025 〢〣〤〥
+
+\\u3105-\\u3107 ㄅㄆ
+\\u310a ㄊ
+\\u3111 ㄑ
+\\u3113 ㄓ
+\\u3116-\\u3117 ㄖㄗ
+\\u311a-\\u311b ㄚㄛ
+\\u3122 ㄢ
+\\u3125 ㄥ
+\\u3127-\\u3128 ㄧㄨ
+
+\\u3220-\\u3229 ㈠㈡㈢㈣㈤㈥㈦㈧㈨
+\\u32a3 ㊣
+\\u338e-\\u338f ㎎㎏
+\\u339c-\\u339d ㎜㎝
+\\u33a1 ㎡
+\\u33a5 ㎥
+\\u33d5 ㏕
+\\u33d1-\\u33d2 ㏑㏒
+
+\\u359e 㖞
+
+\\u39d1 㧑
+
+\\u41f2 䇲
+
+\\u4341 䍁
+
+\\u4d13 䴓
+\\u4d15 䴕
+
 \\u4e00-\\u9fff CJK
-\\uff01-\\uff52 全角
 
+\\uacf3 곳 朝鲜++
 
-\u0020-\u007e
-\u00a0-\u00fe
+\\ucd38 촸 朝鲜++
 
-\u0001
-\u000a
-\u0084
+\\ue20c-\\ue2ff ???
 
+\\uf900-\\ufaff CJK ++
 
+\\ufb03 ﬃ
 
-\u010c-\u010e
-\u0110-\u0111
-\u014c-\u014d
-\u015f-\u0161
-\u0178-\u017a
-\u02c6-\u02cb
-\u02d8-\u02da
-\u0384-\u0385
-\u0387-\u0388
-\u038e-\u038f
-\u0391-\u03a1
-\u03a3-\u03aa
-\u03b1-\u03c9
-\u0410-\u0418
-\u041a-\u0427
-\u042e-\u044f
-\u2018-\u2021
-\u2024-\u2026
-\u2032-\u2034
-\u2039-\u203b
-\u2074-\u2075
-\u2077-\u2078
-\u2160-\u216b
-\u2170-\u2179
-\u2190-\u2193
-\u2211-\u2212
-\u2217-\u221a
-\u221d-\u2220
-\u2227-\u222b
-\u223c-\u223d
-\u2260-\u2261
-\u2264-\u2267
-\u2460-\u2469
-\u2474-\u2481
-\u2487-\u248e
-\u2490-\u2493
-\u2499-\u249b
-\u2501-\u2502
-\u2504-\u2506
-\u2510-\u2511
-\u2514-\u2515
-\u251c-\u251d
-\u251f-\u2520
-\u2526-\u2527
-\u252f-\u2530
-\u253b-\u253c
-\u2550-\u2551
-\u255e-\u255f
-\u2571-\u2573
-\u25a0-\u25a1
-\u25b2-\u25b4
-\u25c6-\u25c7
-\u25ca-\u25cb
-\u25ce-\u25cf
-\u2605-\u2606
-\u266a-\u266b
-\u3001-\u3002
-\u3005-\u300b
-\u300e-\u3011
-\u3013-\u3017
-\u301c-\u301e
-\u3022-\u3025
-\u3105-\u3107
-\u3116-\u3117
-\u311a-\u311b
-\u3127-\u3128
-\u3220-\u3229
-\u338e-\u338f
-\u339c-\u339d
-\u33d1-\u33d2
-\u4e00-\u9fff
-\ue20c-\ue20d
-\ue215-\ue216
-\ue21f-\ue220
-\ue227-\ue229
-\ue233-\ue236
-\ue23a-\ue23b
-\ue244-\ue247
-\ue249-\ue24a
-\ue24d-\ue24e
-\ue25e-\ue25f
-\ue261-\ue262
-\ue265-\ue267
-\ue27b-\ue27c
-\ue2a1-\ue2a2
-\ue2ba-\ue2bc
-\ue2c2-\ue2c4
-\ue2c7-\ue2c9
-\ue2e5-\ue2e7
-\uf918-\uf919
-\uf937-\uf939
-\uf94e-\uf94f
-\uf961-\uf962
-\uf966-\uf967
-\uf969-\uf96c
-\uf96e-\uf970
-\uf978-\uf979
-\uf97b-\uf97c
-\uf99f-\uf9a0
-\uf9b3-\uf9b5
-\uf9b9-\uf9ba
-\uf9bd-\uf9be
-\uf9d0-\uf9d1
-\uf9f3-\uf9f4
-\uf9f6-\uf9f7
-\uf9f9-\uf9fa
-\ufa00-\ufa01
-\ufa06-\ufa0b
-\ufe30-\ufe31
-\ufe3c-\ufe3d
-\ufe3f-\ufe41
-\ufe4d-\ufe4e
-\ufe55-\ufe57
-\ufe59-\ufe5c
-\ufe65-\ufe66
-\ufe6a-\ufe6b
-\uff08-\uff09
-\uffe0-\uffe1
+\\ufe30-\\ufe31 ︰︱
+\\ufe33 ︳
+\\ufe38 ︸
+\\ufe3c-\\ufe3d ︼︽
+\\ufe3f-\\ufe41 ︿﹀﹁
+\\ufe4d-\\ufe4e ﹍﹎
+\\ufe55-\\ufe57 ﹕﹖﹗
+\\ufe59-\\ufe5c ﹙﹚﹛
+\\ufe5f ﹟
+\\ufe63 ﹣
+\\ufe65-\\ufe66 ﹥﹦
+\\ufe6a-\\ufe6b ﹪﹫
+\\ufeff arabic ++ # FE70-FEFF
 
-\u0101
-\u0107
-\u0113
-\u0115
-\u0117
-\u0119
-\u011b
-\u011f
-\u0127
-\u012b
-\u012e
-\u0142
-\u0144
-\u0148
-\u0153
-\u0159
-\u015b
-\u016b
-\u017c
-\u017e
-\u01ce
-\u01d0
-\u01d2
-\u01d4
-\u01d6
-\u01d8
-\u01da
-\u01dc
-\u0251
-\u025b
-\u0261
-\u028a
-\u02d0
-\u02dc
-\u0313
-\u0336
-\u037a
-\u037e
-\u038a
-\u038c
-\u03cb
-\u03d6
-\u0404
-\u0406
-\u0429
-\u042c
-\u0451
-\u04e8
-\u05b2
-\u05be
-\u05d2
-\u0652
-\u11bc
-\u1868
-\u1d31
-\u1d52
-\u1d5b
-\u1ef7
-\u2016
-\u2030
-\u203e
-\u206a
-\u2070
-\u2082
-\u2084
-\u20ac
-\u2103
-\u2105
-\u2109
-\u2116
-\u2122
-\u212b
-\u21d2
-\u2206
-\u2208
-\u2223
-\u2225
-\u222e
-\u2234
-\u2237
-\u2245
-\u224c
-\u2252
-\u226f
-\u2295
-\u2299
-\u22a5
-\u22bf
-\u2312
-\u2395
-\u2483
-\u2485
-\u2495
-\u250c
-\u2524
-\u252a
-\u2534
-\u2537
-\u2539
-\u256b
-\u2584
-\u25aa
-\u25b7
-\u25ba
-\u25bd
-\u25c4
-\u25e6
-\u2609
-\u2610
-\u2640
-\u2642
-\u2666
-\u2714
-\u2717
-\u274f
-\u2751
-\u279f
-\u27a2
-\u27a5
-\u2a7d
-\u2fd4
-\u310a
-\u3111
-\u3113
-\u3122
-\u3125
-\u32a3
-\u33a1
-\u33a5
-\u33d5
-\u359e
-\u39d1
-\u41f2
-\u4341
-\u4d13
-\u4d15
-\uacf3
-\ucd38
-\ue203
-\ue207
-\ue219
-\ue230
-\ue240
-\ue242
-\ue250
-\ue25c
-\ue26a
-\ue26d
-\ue276
-\ue280
-\ue28a
-\ue28c
-\ue292
-\ue294
-\ue29d
-\ue29f
-\ue2a4
-\ue2ad
-\ue2b2
-\ue2b7
-\ue2be
-\ue2d3
-\ue2db
-\ue2f0
-\ue2f3
-\ue2f9
-\ue2ff
-\uf892
-\uf8f5
-\uf901
-\uf907
-\uf90a
-\uf90c
-\uf914
-\uf927
-\uf92d
-\uf934
-\uf941
-\uf949
-\uf952
-\uf95a
-\uf95f
-\uf976
-\uf97e
-\uf981
-\uf98a
-\uf98c
-\uf98e
-\uf99a
-\uf99c
-\uf9a3
-\uf9c1
-\uf9ca
-\uf9cd
-\uf9d4
-\uf9d7
-\uf9dd
-\uf9e0
-\uf9e2
-\uf9e4
-\uf9ea
-\uf9f1
-\uf9fc
-\ufa19
-\ufa1d
-\ufa26
-\ufa29
-\ufb03
-\ufe33
-\ufe38
-\ufe5f
-\ufe63
-\ufeff
-\uff01
-\uff0c
-\uff1a
-\uff1f
-\uff61
-\uff63
-\uff65
-\uff6c
-\uff72
-\uff86
-\uff89
-\uffe3
-\uffe5
-\uffed
-\ufffc
-\u8c4f
-
-
-
+\\uff01 ！
+\\uff08-\\uff09 （）
+\\uff0c ，
+\\uff1a ：
+\\uff1f ？
+\\uff61 ｡
+\\uff63 ｣
+\\uff65 ･
+\\uff6c ｬ
+\\uff72 ｲ
+\\uff86 ﾆ
+\\uff89 ﾉ
+\\uffe0-\\uffe1 ￠￡
+\\uffe3 ￣
+\\uffe5 ￥
+\\uffed ￭
+\\ufffc ￼
 """
